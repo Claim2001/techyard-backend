f59e403f0f0d83e006f24024cc60ce51e3006bbb
Claim2001 <akmal.musaev.2001@mail.ru>
2022-04-18 14:08:22 +0500

    add

---
+++ e_shop/settings_prod.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'django',
+        'PASSWORD': 'c261c4da7a039ac544334c42b1456a65',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
